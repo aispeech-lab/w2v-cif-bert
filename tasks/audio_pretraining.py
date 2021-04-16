b939c656c447394bf82a2477378fe28e8d19475e
eastonYi <yicheng1994@outlook.com>
2021-04-16 16:47:36 +0800

    add tasks

---
+++ tasks/audio_pretraining.py
@@ -0,0 +1,81 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the LICENSE file in
+# the root directory of this source tree. An additional grant of patent rights
+# can be found in the PATENTS file in the same directory.
+
+import os
+import sys
+
+from fairseq.data import FileAudioDataset
+from . import FairseqTask, register_task
+
+
+@register_task("audio_pretraining")
+class AudioUnsuperviseTrainingTask(FairseqTask):
+
+    @staticmethod
+    def add_args(parser):
+        """Add task-specific arguments to the parser."""
+        parser.add_argument("data", help="path to data directory")
+        parser.add_argument(
+            "--sample-rate",
+            default=16000,
+            type=int,
+            help="target sample rate. audio files will be up/down sampled to this rate",
+        )
+        parser.add_argument(
+            "--normalize",
+            action="store_true",
+            help="if set, normalizes input to have 0 mean and unit variance",
+        )
+        parser.add_argument(
+            "--max-sample-size",
+            default=None,
+            type=int,
+            help="max sample size to crop to for batching. default = min sample length",
+        )
+        parser.add_argument(
+            "--min-sample-size",
+            default=None,
+            type=int,
+            help="min sample size to crop to for batching. default = same as --max-sample-size",
+        )
+
+        parser.add_argument(
+            "--enable-padding",
+            action="store_true",
+            help="pad shorter samples instead of cropping",
+        )
+
+    def load_dataset(self, split, **kwargs):
+        """Load a given dataset split.
+
+        Args:
+            split (str): name of the split (e.g., train, valid, test)
+        """
+        manifest = os.path.join(self.args.data, "{}.tsv".format(split))
+        self.datasets[split] = FileAudioDataset(
+            manifest,
+            sample_rate=self.args.sample_rate,
+            max_sample_size=self.args.max_sample_size,
+            min_sample_size=self.args.max_sample_size,
+            min_length=self.args.min_sample_size,
+            pad=self.args.enable_padding,
+            normalize=self.args.normalize,
+        )
+
+    def max_positions(self):
+        """Maximum input length supported by the encoder."""
+        return (sys.maxsize, sys.maxsize)
+
+    def filter_indices_by_size(
+            self,
+            indices,
+            dataset,
+            max_positions=None,
+            ignore_invalid_inputs=False,
+    ):
+        # we do not need to filter by size in this task as dataloaders take care of this
+        return indices
